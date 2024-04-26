2917517d3315e0ad1430f753c6d1fdb4ae84ec31
Timothy S. Williams <tiwillia@redhat.com>
2024-04-26 11:31:58 -0400

    Resolve several minor issues with generator script (#38)
    * Resolve dep issues in templates to the correct repo
    
    * Remove ensemble references
    
    * Add missing present helpers
    
    * Remove reference to un-generated validation method
    
    * Add object metadata fields to ObjectRefernce

--- pkg/util/utils.go
+++ pkg/util/utils.go
@@ -5,6 +5,34 @@ import (
 	"fmt"
 )
 
+// ToPtr returns a pointer copy of value.
+func ToPtr[T any](v T) *T {
+	return &v
+}
+
+// FromPtr returns the pointer value or empty.
+func FromPtr[T any](v *T) T {
+	if v == nil {
+		return Empty[T]()
+	}
+	return *v
+}
+
+// FromEmptyPtr emulates ToPtr(FromPtr(x)) sequence
+func FromEmptyPtr[T any](v *T) *T {
+	if v == nil {
+		x := Empty[T]()
+		return &x
+	}
+	return v
+}
+
+// Empty returns an empty value of type T.
+func Empty[T any]() T {
+	var zero T
+	return zero
+}
+
 func EmptyStringToNil(a string) *string {
 	if a == "" {
 		return nil
