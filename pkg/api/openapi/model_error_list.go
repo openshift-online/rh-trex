7457f4c763b4fc2e83d7bf751ebb55608fa0dd77
Angel Marin <amarin@redhat.com>
2025-10-08 18:45:09 +0200

    add openapi artifacts

--- pkg/api/openapi/model_error_list.go
+++ pkg/api/openapi/model_error_list.go
@@ -11,7 +11,9 @@ API version: 0.0.1
 package openapi
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 )
 
 // checks if the ErrorList type satisfies the MappedNullable interface at compile time
@@ -25,6 +27,8 @@ type ErrorList struct {
 	Total int32   `json:"total"`
 	Items []Error `json:"items"`
 }
+
+type _ErrorList ErrorList
 
 // NewErrorList instantiates a new ErrorList object
 // This constructor will assign default values to properties that have it defined,
@@ -184,6 +188,47 @@ func (o ErrorList) ToMap() (map[string]interface{}, error) {
 	toSerialize["total"] = o.Total
 	toSerialize["items"] = o.Items
 	return toSerialize, nil
+}
+
+func (o *ErrorList) UnmarshalJSON(data []byte) (err error) {
+	// This validates that all required properties are included in the JSON object
+	// by unmarshalling the object into a generic map with string keys and checking
+	// that every required field exists as a key in the generic map.
+	requiredProperties := []string{
+		"kind",
+		"page",
+		"size",
+		"total",
+		"items",
+	}
+
+	allProperties := make(map[string]interface{})
+
+	err = json.Unmarshal(data, &allProperties)
+
+	if err != nil {
+		return err
+	}
+
+	for _, requiredProperty := range requiredProperties {
+		if _, exists := allProperties[requiredProperty]; !exists {
+			return fmt.Errorf("no value given for required property %v", requiredProperty)
+		}
+	}
+
+	varErrorList := _ErrorList{}
+
+	decoder := json.NewDecoder(bytes.NewReader(data))
+	decoder.DisallowUnknownFields()
+	err = decoder.Decode(&varErrorList)
+
+	if err != nil {
+		return err
+	}
+
+	*o = ErrorList(varErrorList)
+
+	return err
 }
 
 type NullableErrorList struct {
